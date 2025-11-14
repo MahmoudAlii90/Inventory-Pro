40b7803d58ba3a7ae3ef6ef958ef010051d66ed1
MahmoudAlii90 <118017456+MahmoudAlii90@users.noreply.github.com>
2025-11-14 13:32:12 +0200

    Delete Inventory Pro directory

--- Inventory Pro/ui/global_signals.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtCore import QObject, pyqtSignal
-
-class GlobalSignals(QObject):
-    # إشارة عامة تُرسل عند أي تغيير في البيانات
-    data_changed = pyqtSignal()
-
-# كائن عالمي يستخدمه كل الصفحات
-global_signals = GlobalSignals()
